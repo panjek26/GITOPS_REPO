8b78bc2a4efafd52fb701fe644c1e8b9db279dba
panji.kurniawan <panjipratamakurniawan001@gmail.com>
2023-04-07 06:45:02 +0700

    Update generate_secret.sh

--- utiltiy/generate_secret.sh
+++ utiltiy/generate_secret.sh
@@ -3,9 +3,8 @@
 KEY_ENV=prod
 KEY_NAME=abc-conn-secret
 
-kubectl create secret generic postgres-secret --dry-run=client \
-	--from-literal=username=panji \
-	--from-literal=password=123 \
+kubectl create secret generic dbpwd --dry-run=client \
+	--from-literal=password=panji \
 	--output=yaml > ./${KEY_NAME}-${KEY_ENV}-opaque.yaml
 
 kubeseal --format yaml --scope cluster-wide < ./${KEY_NAME}-${KEY_ENV}-opaque.yaml > ./${KEY_NAME}-${KEY_ENV}-sealed.yaml
